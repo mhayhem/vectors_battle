0445cd12892de52527a0e794cae661acad344dc4
MhayhemDev <mhayhemdev@gmail.com>
2025-06-21 15:43:57 +0100

    deleted license file

--- code/main.py
+++ code/main.py
@@ -1,4 +1,4 @@
-import figthers_class as fc
+from figthers_class import Figthers as fc
 import random
 
 infantry = fc(40, 6, 4)
@@ -6,6 +6,7 @@ calvary = fc(60, 8, 6)
 catapult = fc(80, 10, 8)
 
 types_fighters = [infantry, calvary, catapult]
+
 
 def created_army(list_figthers):
     army_1 = []
@@ -15,10 +16,11 @@ def created_army(list_figthers):
         army_1.append(fighter)
         fighter = random.choice(types_fighters)
         army_2.append(fighter)
-    return army_1, army_2
+    return select_fighters(army_1, army_2)
 
 def select_fighters(army_1, army_2):
     # take first element of array as fighters
     figther_1 = army_1.pop(0)
     figther_2 = army_2.pop(0)
-        
+    
+
