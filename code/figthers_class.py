8e3e55d7a54c3feabdecfd1988336792f03134a5
MhayhemDev <mhayhemdev@gmail.com>
2025-06-21 13:55:45 +0100

    added folder code and .gitignore

---
+++ code/figthers_class.py
@@ -0,0 +1,7 @@
+class Figthers:
+    def __init__(self, life, attack, defense):
+        self.life = int(life)
+        self.attack = int(attack)
+        self.defense = int(defense)
+        
+    
---
+++ code/main.py
@@ -0,0 +1,24 @@
+import figthers_class as fc
+import random
+
+infantry = fc(40, 6, 4)
+calvary = fc(60, 8, 6)
+catapult = fc(80, 10, 8)
+
+types_fighters = [infantry, calvary, catapult]
+
+def created_army(list_figthers):
+    army_1 = []
+    army_2 = []
+    for i in range(10):
+        fighter = random.choice(types_fighters)
+        army_1.append(fighter)
+        fighter = random.choice(types_fighters)
+        army_2.append(fighter)
+    return army_1, army_2
+
+def select_fighters(army_1, army_2):
+    # take first element of array as fighters
+    figther_1 = army_1.pop(0)
+    figther_2 = army_2.pop(0)
+        
